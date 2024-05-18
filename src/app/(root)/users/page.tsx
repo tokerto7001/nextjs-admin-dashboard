36748cfd04414b50255f4e868a0d6ae48f33408d
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-05-18 17:52:41 +0300

    feat: add pagination for users page and refactor shacdn ui element

--- src/app/(root)/users/page.tsx
+++ src/app/(root)/users/page.tsx
@@ -8,16 +8,38 @@ import {
     TableHeader,
     TableRow,
   } from "@/components/ui/table"
+import { redirect } from "next/navigation";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
   
+interface UsersPageProps {
+    searchParams: {
+        page: string;
+    }
+}
+export default async function UsersPage({searchParams}: UsersPageProps){
 
-export default async function UsersPage(){
+    const { page = 1 } = searchParams;
+    if(isNaN(+page)) redirect('/users')
+    const offset = (+page - 1) * 10; 
+
+    // console.log(+page - 1 < 1)
 
     const users = await db.user.findMany({
         select: {
             email: true,
             id: true,
             isAdmin: true
-        }
+        },
+        skip: offset,
+        take: 10
     });
 
     return (
@@ -44,7 +66,16 @@ export default async function UsersPage(){
 
             </TableBody>
             </Table>
-
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                    <PaginationPrevious disabled={+page - 1 < 1} href={`/users?page=${+page - 1}`} />
+                    </PaginationItem>
+                    <PaginationItem>
+                    <PaginationNext disabled={users.length < 10} href={`/users?page=${+page + 1}`} />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </div>
     )
 };
